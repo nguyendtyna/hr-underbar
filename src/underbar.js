bd26e5b7a819f887da15ce286b4f093c2bb27863
Tyna Nguyen <ngtyna@gmail.com>
2020-08-31 19:18:27 -0500

    Implement _.uniq to pass

--- src/underbar.js
+++ src/underbar.js
@@ -141,6 +141,31 @@
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array, isSorted, iterator) {
+    let uniqueResults = [];
+
+    if (isSorted === true) {
+      // iterate over input array
+      for (let i = 0; i < array.length; i++) {
+        let item = array[i];
+        let lastItem = uniqueResults.length - 1;
+        let isUnique = iterator(uniqueResults[lastItem]) !== iterator(item);
+        // check if the current item is found in the array and is unique
+        if (_.indexOf(uniqueResults, item) === -1 && (isUnique)) {
+          uniqueResults.push(item);
+        }
+      }
+    } else {
+      // perform without isSorted
+      for (let i = 0; i < array.length; i++) {
+        let item = array[i];
+        if (_.indexOf(uniqueResults, item) === -1) {
+          uniqueResults.push(item);
+        }
+      }
+    }
+
+    // return array that does not contain duplicate elements
+    return uniqueResults;
   };
 
 
